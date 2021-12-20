87770083912a14b1a2a8210a35b70ef29e2f8dbe
Jorge Luis Jimenez <jorge@Jorges-MacBook-Pro.local>
2021-12-20 16:27:35 -0500

    creating exercises logic for db v0.1

---
+++ models/exercise.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const ExerciseSchema = new mongoose.Schema({
+
+    userID: {
+        type: "String",
+        required: true
+    },
+    description: {
+        type: "String",
+        required: true
+    },
+    duration: {
+        type: "Number",
+        required: true
+    },
+    date: {
+        type: "Date",
+        required: true
+    }
+})
+
+
+
+module.exports = mongoose.model("Exercise", ExerciseSchema);
